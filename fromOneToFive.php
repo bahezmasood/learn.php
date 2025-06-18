e37a1bd8ca51320312b5ba04b783d1dcef26adf5
bahezmasood <bahezsoftwareeng@gmail.com>
2025-06-18 19:36:57 +0300

    Create fromOneToFive.php
    on k97 with b06

---
+++ fromOneToFive.php
@@ -0,0 +1,52 @@
+<?php
+echo "all of them is good in our ife  </br>";
+echo "<h1> dll ba hiswa</h1>way way dll ba 7ishwa danala";
+$name = "</br> ahmed";
+echo$name;
+$a=strlen("lsdjf");
+echo$a;
+$lk=str_replace("bahez masodu", "abdullah hamad", "bahez masodu");
+echo $a;
+echo $lk;
+$laravel=str_word_count("hello , how are you , you are good inshallah");
+echo$laravel;
+
+?>
+<?php 
+echo chop("hey hey dll daswty danaleny ","hey");
+$bahez = str_replace("bahez","","bahez maosud abdullah");
+$bahez1=chop("lanew arzy waq7 da 7 ","lanew");
+echo $bahez1;
+echo $bahez;
+?>
+<?php 
+$array = array("tara","shara",54,45);
+print_r($array);
+?>
+<?php 
+$array = array("tara","shara",54,45);
+print_r($array);
+echo "</br>".$array[3];
+?>
+<?php 
+$ARRAY = array("tara"=>"lara","shara"=>"mara",54=>"haman",45=>"7aman");
+print_r($ARRAY);
+ARRAY_pop($ARRAY);
+ARRAY_push($ARRAY,"noway");
+print_r($ARRAY);
+?>
+<?php 
+$ARRAY = array("tara"=>"lara","shara"=>"mara",54=>"haman",45=>"7aman");
+print_r($ARRAY);
+echo("</br>");
+ARRAY_unshift($ARRAY,"bahez maodu","ldjf");
+print_r($ARRAY);
+?>
+<?php 
+$ARRAY = array("tara"=>"lara","shara"=>"mara",54=>"haman",45=>"7aman");
+print_r($ARRAY);
+echo("</br>");
+ARRAY_unshift($ARRAY,"bahez maodu","ldjf");
+asort($ARRAY);
+print_r($ARRAY);
+?>
